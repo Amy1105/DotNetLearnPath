f3bd8f4af47853b7c6b3a25bd4be9aadd9e99367
zhuyingying <yingying.zhu@cn.aesc-group.com>
2025-07-15 17:12:50 +0800

    add test demo

---
+++ TestDemo/TryCatchDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+string s = "123";
+
+if(double.TryParse(s,out double d))
+{
+    Console.WriteLine(d);
+}
+else
+{
+    Console.WriteLine($"{s}无法转换成double类型");
+}
+
+
+
+try
+{
+   var dd= Convert.ToDouble(s);
+}
+catch(Exception e)
+{
+    Console.WriteLine($"{s}无法转换成double类型;"+e.Message);
+}
